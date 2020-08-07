0a1d4da844942972dce21c820df919c20d0b829b
Carlos Teixeira <carlosteixeira@MacBook-Air-de-Carlos.local>
2020-08-07 21:39:05 +0200

    Adding Carlos personal page to the personal branch

---
+++ js/script.js
@@ -0,0 +1,17 @@
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+ }
+
+
+ /*body {
+  padding: 25px;
+  background-color: white;
+  color: black;
+  font-size: 25px;
+}
+
+.dark-mode {
+  background-color: black;
+  color: white;
+}*/
